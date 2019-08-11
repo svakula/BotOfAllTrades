e93eddfb509cfd90486590a374db011f1caad4c6
shrijith vakula <36484784+spigletable@users.noreply.github.com>
2019-08-10 20:27:55 -0700

    init fixes & code simplifiying
    title

--- index.js
+++ index.js
@@ -4,48 +4,15 @@ const token = 'NjA5NjE0Nzc1OTMyMDI2OTAy.XU5zeA.CHES5tmHjiOAkp9vAAPd-bexcBE';
 const client = new Discord.Client();
 
 client.on('message', (msg) => {
-    if (msg.content === 'fuck ') {
-        msg.channel.send(`Not On My Christian Discord Server ${msg.author}`);
-    }
-});
-
-client.on('message', (msg) => {
-    if (msg.content === 'shit ') {
-        msg.channel.send(`Not On My Christian Discord Server ${msg.author}`);
-    }
-});
-
-client.on('message', (msg) => {
-    if (msg.content === 'ass ') {
-        msg.channel.send(`Not On My Christian Discord Server ${msg.author}`);
-    }
-});
-client.on('message', (msg) => {
-    if (msg.content === 'nigga ') {
-        msg.channel.send(`Not On My Christian Discord Server ${msg.author}`);
-    }
-});
-
-client.on('message', (msg) => {
-    if (msg.content === 'fucking ') {
-        msg.channel.send(`Not On My Christian Discord Server ${msg.author}`);
-    }
-});
-
-client.on('message', (msg) => {
-    if (msg.content === 'hell ') {
-        msg.channel.send(`Not On My Christian Discord Server ${msg.author}`);
-    }
-});
-
-client.on('message', (msg) => {
-    if (msg.content === 'frick ') {
+    if (msg.content === 'word1' || msg.content === 'word2' || msg.content === 'word3') {
         msg.channel.send(`Not On My Christian Discord Server ${msg.author}`);
     }
 });
 
 client.on('ready', () => {
-    console.log('bot works somehow');
+    console.log('Its Online now Dumbass');
+    
+   // client.channels.find(x => x.name === 'test').send('hello i now exist');
 });
 
 client.login(token);
