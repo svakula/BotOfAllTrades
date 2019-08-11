f10e68d2a7749b816368e8054b16e278535f39c7
shrijith vakula <36484784+spigletable@users.noreply.github.com>
2019-08-10 20:49:59 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,22 @@ const Discord = require('discord.js');
 const token = 'NjA5NjE0Nzc1OTMyMDI2OTAy.XU5zeA.CHES5tmHjiOAkp9vAAPd-bexcBE';
 
 const client = new Discord.Client();
-
+//replace words with actual words that you want to send warning to
+//replace warning with any warning but I think the current warning is pretty good
 client.on('message', (msg) => {
     if (msg.content === 'word1' || msg.content === 'word2' || msg.content === 'word3') {
         msg.channel.send(`Not On My Christian Discord Server ${msg.author}`);
     }
 });
 
+client.on('message', (msg) => {
+    if (msg.content === 'lmao' || msg.content === 'lmfao' || msg.content === 'lnao' || msg.content === 'Inao' || msg.content === 'Imao') {
+        msg.channel.send(`Take Yo DUMB Ass Back ${msg.author}`);
+    }
+});
+
 client.on('ready', () => {
     console.log('Its Online now Dumbass');
-    
-   // client.channels.find(x => x.name === 'test').send('hello i now exist');
 });
 
 client.login(token);
