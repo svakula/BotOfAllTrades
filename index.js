af67a55416cfc50ee935156f87c05aa7aa555b16
Shrijith Vakula <svakula31@gmail.com>
2019-08-11 20:54:54 -0700

    Fixed Array List

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const Discord = require('discord.js');
-const token = 'NjA5NjE0Nzc1OTMyMDI2OTAy.XU-m2g.NqQm794kHqAmTX44mHrkUyAMDnY';
+const token = 'NjA5NjE0Nzc1OTMyMDI2OTAy.XVDV5w.2OdpsqjkR7DYrm2GIxi2iY8fbpY';
 
 const client = new Discord.Client();
+
+const words = ['word1' , 'word2' , 'word3'];
 //replace words with actual words that you want to send warning to
 //replace warning with any warning but I think the current warning is pretty good
 client.on('message', (msg) => {
-    if (msg.content === 'word1' || msg.content === 'word2' || msg.content === 'word3') {
-        msg.channel.send(`Not On My Christian Discord Server ${msg.author}`);
+    //if (msg.content === 'word1' || msg.content === 'word2' || msg.content === 'word3') {
+    console.log(' Am I here ? '+words[0]);
+    for (i=0; i<words.length; i++) {
+    if (msg.content.indexOf(words[i]) > -1) {
+        msg.channel.send(`Statement* ${msg.author}`);
+    }
     }
 });
 
 client.on('message', (msg) => {
-    if (msg.content === 'lmao' || msg.content === 'lmfao' || msg.content === 'lnao' || msg.content === 'Inao' || msg.content === 'Imao') {
-        msg.channel.send(`Take Yo DUMB Ass Back ${msg.author}`);
+    if (msg.content === 'Word4' || msg.content === 'Word5' || msg.content === 'Word6' || msg.content === 'Word7' || msg.content === 'Word8') {
+        msg.channel.send(`Statement** ${msg.author}`);
     }
 });
 
