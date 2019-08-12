4282b685acb8dad4451d026a3ed0e77785385a2e
Shrijith Vakula <svakula31@gmail.com>
2019-08-11 21:08:22 -0700

    Added Array2 and fixed statements
     Lmao Update    --    no s-words yet

--- index.js
+++ index.js
@@ -3,22 +3,27 @@ const token = 'NjA5NjE0Nzc1OTMyMDI2OTAy.XVDV5w.2OdpsqjkR7DYrm2GIxi2iY8fbpY';
 
 const client = new Discord.Client();
 
-const words = ['word1' , 'word2' , 'word3'];
+const swords = ['word1' , 'word2' , 'word3'];
+const lmaos = ['lmao' , 'lmfao' , 'lnao' , 'Imao' , 'Imfao' , 'Inao'];
 //replace words with actual words that you want to send warning to
 //replace warning with any warning but I think the current warning is pretty good
 client.on('message', (msg) => {
-    //if (msg.content === 'word1' || msg.content === 'word2' || msg.content === 'word3') {
-    console.log(' Am I here ? '+words[0]);
-    for (i=0; i<words.length; i++) {
-    if (msg.content.indexOf(words[i]) > -1) {
-        msg.channel.send(`Statement* ${msg.author}`);
-    }
+    console.log(' Am I here ? '+swords[0]);
+    for (i=0; i<swords.length; i++) {
+        if (msg.content.indexOf(swords[i]) > -1) {
+            msg.channel.send(`Not On My Christian Discord Server ${msg.author}`);
+            break;
+        }
     }
 });
 
 client.on('message', (msg) => {
-    if (msg.content === 'Word4' || msg.content === 'Word5' || msg.content === 'Word6' || msg.content === 'Word7' || msg.content === 'Word8') {
-        msg.channel.send(`Statement** ${msg.author}`);
+    console.log(' Am I here ? '+lmaos[0]);
+    for (i=0; i<lmaos.length; i++) {
+        if (msg.content.indexOf(lmaos[i]) > -1) {
+            msg.channel.send(`Take Yo DUMB Ass Back ${msg.author}`);
+            break;
+        }
     }
 });
 
