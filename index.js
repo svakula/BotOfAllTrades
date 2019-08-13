8812b3e722d3f4587152a3397467d587fb9f19f6
Shrijith Vakula <svakula31@gmail.com>
2019-08-12 17:51:43 -0700

    Removed Console Logs

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const lmaos = ['lmao' , 'lmfao' , 'lnao' , 'Imao' , 'Imfao' , 'Inao'];
 //replace words with actual words that you want to send warning to
 //replace warning with any warning but I think the current warning is pretty good
 client.on('message', (msg) => {
-    console.log(' ActivityMonitor >>> '+swords[0]);
     for (i=0; i<swords.length; i++) {
         if (msg.content.indexOf(swords[i]) > -1) {
             msg.channel.send(`Not On My Christian Discord Server ${msg.author}`);
@@ -18,7 +17,6 @@ client.on('message', (msg) => {
 });
 
 client.on('message', (msg) => {
-    console.log(' ActivityMonitor >>> '+lmaos[0]);
     for (i=0; i<lmaos.length; i++) {
         if (msg.content.indexOf(lmaos[i]) > -1) {
             msg.channel.send(`Take Yo DUMB Ass Back ${msg.author}`);
